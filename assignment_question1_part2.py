3b66616d92cce733ccbe7d4d1ec7dd1abf5b7f2a
Itay Rosen <itayrosen04@gmail.com>
2026-01-14 16:46:21 +0200

    finished assignment_question1_part1_part2

--- assignment_question1_part2.py
+++ assignment_question1_part2.py
@@ -1,40 +1,90 @@
+class Problem:
+    def __init__(self,name, description):
+        self.name = name
+        self.description = description
+
+    def BusinessLinkage(self, businessId):
+        self.business = Business(businessId)
+
+    def addSeverity(self, severityScore):
+        self.severityScore = severityScore
+        print(f"The severity score is {self.severityScore}")
+
+class Advantage:
+    def __init__(self,name, description):
+        self.name = name
+        self.description = description
+
+    def BusinessLinkage(self, businessId):
+        self.business = Business(businessId)
+
+    def addScore(self, advantageScore):
+        self.advantageScore = advantageScore
+        print(f"The score is {self.advantageScore}")
+
+class Business:
+    business = None
+    def __new__(cls, businessId):
+        if cls.business is not None:
+              return cls.business
+        cls.business = super().__new__(cls)
+        return cls.business
+    def __init__(self, businessId):
+        self.id = businessId
+
+
+class MyProblem(Problem):
+    def __init__(self, name, description, platform):
+        super().__init__(name, description)
+        self.platform = platform
+        self.retryAttempts = 0
+
+class MyAdvantage(Advantage):
+    def __init__(self, name, description, ordersReceived, ordersCompleted):
+        super().__init__(name, description)
+        self.ordersReceived = ordersReceived
+        self.ordersCompleted = ordersCompleted
+
 class Data:
     def __init__(self, businessId):
-        self.businessId = businessId 
+        self.businessId = businessId
         self.info = []
 
+        # Create the raw objects
         problem1 = MyProblem("Error #1", "Connection Lost", "Wolt")
-        problem2 = MyProblem("Error #2", "Connection Lost", "TenBis")
-        advantage1= MyAdvantage("Advantage #1", "Table Report", 51, 49)
+        problem2 = MyProblem("Error #2", "Connection Failure", "TenBis")
+        advantage = MyAdvantage("Advantage", "Table Report", 25, 15)
 
-        self.info.append(ProblemAdapter(problem1))
-        self.info.append(ProblemAdapter(problem2))
-        self.info.append(AdvantageAdapter(advantage1))
+        # Wrap them in adapters and add to list
+        self.info.append(problemAdapter(problem1))
+        self.info.append(problemAdapter(problem2))
+        self.info.append(advantageAdapter(advantage))
+
 
 # Adapter for problems
-class ProblemAdapter:
+class problemAdapter:
     def __init__(self, problem):
-        self.problem = problem
+        self.problem = problem # Stores the original object
 
+    # Translates "updateScore" into "addSeverity"
     def updateScore(self, severityScore):
-        self.problem.AddSeverity(severityScore)
+        self.problem.addSeverity(severityScore)
 
 
 # Adapter for advantages
-class AdvantageAdapter:
+class advantageAdapter:
     def __init__(self, advantage):
-        self.advantage = advantage
+        self.advantage = advantage # Stores the original object
 
+    # Translates "updateScore" into "addScore"
     def updateScore(self, advantageScore):
-        self.advantage.AddScore(advantageScore)
-        
+        self.advantage.addScore(advantageScore)
+
+
 def main():
-    business = MyBusiness(1, ["Wolt", "TenBis", "Mishloha"], "Active")
-    
-    data = Data(business.id)
-    
-    print("--- Running Example: ---")
+    data = Data(69)
+
     for i in data.info:
         i.updateScore(5)
-        print('\n')
+        print("\n")
 main()
