956dbce0400782243e231a57ae22386ca5d24531
Itay Rosen <itayrosen04@gmail.com>
2026-01-14 10:08:17 +0200

    stuff

--- assignment_question1_part2.py
+++ assignment_question1_part2.py
@@ -3,10 +3,12 @@ class Data:
         self.businessId = businessId 
         self.info = []
 
+        # Create the raw objects
         problem1 = MyProblem("Error #1", "Connection Lost", "Wolt")
         problem2 = MyProblem("Error #2", "Connection Lost", "TenBis")
         advantage1= MyAdvantage("Advantage #1", "Table Report", 51, 49)
 
+        # Wrap them in adapters
         self.info.append(ProblemAdapter(problem1))
         self.info.append(ProblemAdapter(problem2))
         self.info.append(AdvantageAdapter(advantage1))
@@ -14,26 +16,27 @@ class Data:
 # Adapter for problems
 class ProblemAdapter:
     def __init__(self, problem):
-        self.problem = problem
-
+        self.problem = problem # Stores the original object
+        
+    # Translates "updateScore" into "addSeverity"
     def updateScore(self, severityScore):
-        self.problem.AddSeverity(severityScore)
+        self.problem.addSeverity(severityScore)
 
 
 # Adapter for advantages
 class AdvantageAdapter:
     def __init__(self, advantage):
-        self.advantage = advantage
+        self.advantage = advantage # Stores the original object
 
+    # Translates "updateScore" into "addScore"
     def updateScore(self, advantageScore):
-        self.advantage.AddScore(advantageScore)
+        self.advantage.addScore(advantageScore)
         
 def main():
     business = MyBusiness(1, ["Wolt", "TenBis", "Mishloha"], "Active")
     
     data = Data(business.id)
-    
-    print("--- Running Example: ---")
+        
     for i in data.info:
         i.updateScore(5)
         print('\n')
